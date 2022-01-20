32cf679f5fb82783796473ff9b856985b35b8e06
Spotted Owelette <ruchita.kulkarni9@gmail.com>
2022-01-19 23:58:20 -0800

    commit at the end of 13.4.2

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -2,7 +2,7 @@
 console.log("working");
 
 // Create the map object with a center and zoom level: method 1
-let map = L.map('mapid').setView([34.0522, -118.2437], 14);
+let map = L.map('mapid').setView([34.0522, -118.2437], 5);
 
 // Create the map object with a center and zoom level: method 2
 // let map = L.map("mapid", {
@@ -12,13 +12,21 @@ let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 //     zoom: 4
 //   });
 
-//  Add a marker to the map for Los Angeles, California.
-let marker = L.circle([34.0522, -118.2437], {
-  radius: 300,
-  color: "black",
-  fillColor: "yellow"
-}).addTo(map);
-//change the marker to a circle
+//get data from cities.js
+let cityData = cities;
+
+// Loop through each city object and create on marker for each city
+cityData.forEach(function(city) {
+  console.log(city);
+  L.circleMarker(city.location, {
+    radius: city.population/100000,
+    color: "orange",
+    fillColor: "orange"
+  })
+  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population: " + city.population.toLocaleString() + "</h3>")
+  .addTo(map);
+});
+
 
 
 // We create the tile layer that will be the background of our map.
