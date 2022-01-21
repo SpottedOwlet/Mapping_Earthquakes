6ba80c168c7d49c9fb9e00c7cd74c7a3ce886e9d
Spotted Owelette <ruchita.kulkarni9@gmail.com>
2022-01-20 16:43:29 -0800

    Lines Mapped

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -2,35 +2,27 @@
 console.log("working");
 
 // Create the map object with a center and zoom level: method 1
-let map = L.map('mapid').setView([34.0522, -118.2437], 5);
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
 
-// Create the map object with a center and zoom level: method 2
-// let map = L.map("mapid", {
-//     center: [
-//       40.7, -94.5
-//     ],
-//     zoom: 4
-//   });
+//Coordinates for each point to be used i a polyline
+let line = [
+  [37.6213, -122.3790],
+  [30.1975, -97.6664], 
+  [43.6777, -79.6248],
+  [40.6413,-73.7781],
+  [42.3656, -71.0096]
+];
 
-//get data from cities.js
-let cityData = cities;
-
-// Loop through each city object and create on marker for each city
-cityData.forEach(function(city) {
-  console.log(city);
-  L.circleMarker(city.location, {
-    radius: city.population/100000,
-    color: "orange",
-    fillColor: "orange"
-  })
-  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population: " + city.population.toLocaleString() + "</h3>")
-  .addTo(map);
-});
-
-
+//Create a polyline using the line coordinates and make the line red
+L.polyline(line,{
+  weight: 4,
+  opacity: 0.5,
+  dashArray: 8,
+  color: "blue"
+}).addTo(map);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
